103ab31c314cf0958812a325ee3b556a8c876ba3
Milan Suwała <metonator@gmail.com>
2017-07-17 01:22:31 +0200

    Fast fix

--- GameLauncher/App/Classes/NativeFunctions.cs
+++ GameLauncher/App/Classes/NativeFunctions.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 
 namespace GameLauncher.App.Classes {
-    internal static class NativeFunctions  {
+    internal static class LZMA  {
         [DllImport("LZMA.dll", CharSet = CharSet.None, ExactSpelling = false)]
         public static extern int LzmaUncompress(byte[] dest, ref IntPtr destLen, byte[] src, ref IntPtr srcLen, byte[] outProps, IntPtr outPropsSize);
 
