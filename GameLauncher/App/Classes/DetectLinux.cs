d3e843b22a37a132d6be8942f778e13f732d20a5
redbluescreen <8455367+redbluescreen@users.noreply.github.com>
2018-10-22 12:44:58 +0300

    Rich Presence distro fixes
    Makes distro check use /etc/os-release instead of lsb_release to be more
    compatible with various distros

--- GameLauncher/App/Classes/DetectLinux.cs
+++ GameLauncher/App/Classes/DetectLinux.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace GameLauncher.App.Classes {
     class DetectLinux {
@@ -39,19 +40,42 @@ namespace GameLauncher.App.Classes {
 
         public static string Distro() {
             if(_linuxDistro == null) {
-                var ps = new ProcessStartInfo
-                {
-                    FileName = "lsb_release",
-                    Arguments = "-d",
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true
-                };
-                var p = Process.Start(ps);
-                _linuxDistro = p.StandardOutput.ReadLine().Replace("Description:", "").Trim();
-                p.StandardOutput.ReadToEnd();
+                _linuxDistro = LinuxDistroInternal();
             }
 
             return _linuxDistro;
+        }
+
+        private static string LinuxDistroInternal() {
+            if (!File.Exists("/etc/os-release"))
+            {
+                return "Linux";
+            }
+            using (var stream = new StreamReader("/etc/os-release"))
+            {
+                string line;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    var splits = line.Split(new[] { '=' }, 2);
+                    if (splits[0] == "PRETTY_NAME")
+                    {
+                        var val = splits[1];
+                        if (val[0] == '"')
+                        {
+                            val = val.Substring(1);
+                        }
+                        if (val[val.Length - 1] == '"')
+                        {
+                            val = val.Substring(0, val.Length - 1);
+                        }
+                        if (val == "Arch Linux" && new Random().NextDouble() < 0.2) {
+                            return "btw i use arch";
+                        }
+                        return val;
+                    }
+                }
+            }
+            return "Linux";
         }
 
         private static string KernelName() {
