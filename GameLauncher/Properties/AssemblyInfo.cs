99e59c60a5cdd33b3c88cb92e4b808f0d91ec9f2
Milan Suwała <metonator@gmail.com>
2019-01-08 13:12:01 +0100

    Update AssemblyInfo.cs

--- GameLauncher/Properties/AssemblyInfo.cs
+++ GameLauncher/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("0a114c5c-3566-4a62-b05d-cba311988d8a")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.0.2.0")]
+[assembly: AssemblyFileVersion("2.0.2.0")]
