545ee95e929b684f1eaa6ce842b151ef7eb42097
metonator <metonator@gmail.com>
2019-02-03 22:56:42 +0100

    Timestamp fix

--- GameLauncher/Properties/AssemblyInfo.cs
+++ GameLauncher/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MTNTR")]
 [assembly: AssemblyProduct("GameLauncher")]
-[assembly: AssemblyCopyright("OpenSource")]
+[assembly: AssemblyCopyright("WorldUnited")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
 // przy użyciu symbolu „*”, tak jak pokazano poniżej:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.2.2")]
-[assembly: AssemblyFileVersion("2.0.2.2")]
+[assembly: AssemblyVersion("2.0.2.3")]
+[assembly: AssemblyFileVersion("2.0.2.3")]
