ddafb5a7d2276809749127b6a0c7ad41c899e836
Zacam <Zacam.1369@gmail.com>
2020-06-07 00:46:37 -0700

    Minor DiscordRPC Updates
    Cars Added:
    Brera, Sard MC8 and Cayenne Turbo S
    Replace "STringray" with "Stingray"
    tab-indent WU cars in list, append all other servers to bottom
    
    Events Added:
    Le Mans, City Perimeter, Rockport World Loop
    Rename "Fortuna Run" to "Palmont Tour"
    
    Update Avatars count index
    (Fixes Issue #47 that was opened)

--- GameLauncher/App/Classes/RPC/DiscordGamePresence.cs
+++ GameLauncher/App/Classes/RPC/DiscordGamePresence.cs
@@ -125,7 +125,7 @@ namespace GameLauncher.App.Classes.RPC {
                     SBRW_XML.LoadXml(serverreply);
                     PersonaName = SBRW_XML.SelectSingleNode("ProfileData/Name").InnerText.Replace("¤", "[S]");
                     PersonaLevel = SBRW_XML.SelectSingleNode("ProfileData/Level").InnerText;
-                    PersonaAvatarId = (SBRW_XML.SelectSingleNode("ProfileData/IconIndex").InnerText == "26") ? "nfsw" : "avatar_" + SBRW_XML.SelectSingleNode("ProfileData/IconIndex").InnerText;
+                    PersonaAvatarId = (SBRW_XML.SelectSingleNode("ProfileData/IconIndex").InnerText == "94") ? "nfsw" : "avatar_" + SBRW_XML.SelectSingleNode("ProfileData/IconIndex").InnerText;
                     PersonaId = SBRW_XML.SelectSingleNode("ProfileData/PersonaId").InnerText;
 
                     AntiCheat.persona_name = SBRW_XML.SelectSingleNode("ProfileData/Name").InnerText.Replace("¤", "[S]");
