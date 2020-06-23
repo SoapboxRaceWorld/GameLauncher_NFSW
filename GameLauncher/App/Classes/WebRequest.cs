56011ac718cf246871fa982e499e085019615114
MeTonaTOR <metonator@gmail.com>
2020-06-23 19:53:00 +0200

    DisableProxy enhancement

--- GameLauncher/App/Classes/WebRequest.cs
+++ GameLauncher/App/Classes/WebRequest.cs
@@ -29,9 +29,9 @@ namespace GameLauncherReborn {
             }
             
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(address);
-            request.UserAgent = (Self.userAgent == null) ? "GameLauncher (+https://github.com/SoapboxRaceWorld/GameLauncher_NFSW)" : Self.userAgent;
+            request.UserAgent = (Self.userAgent == null) ? "GameLauncher (+https://github.com/worldunitedgg/GameLauncher_NFSW)" : Self.userAgent;
             request.Headers["X-HWID"] = Security.FingerPrint.Value();
-            request.Headers["X-UserAgent"] = "GameLauncherReborn "+Application.ProductVersion+ " WinForms (+https://github.com/SoapboxRaceWorld/GameLauncher_NFSW)";
+            request.Headers["X-UserAgent"] = "GameLauncherReborn "+Application.ProductVersion+ " WinForms (+https://github.com/worldunitedgg/GameLauncher_NFSW)";
             request.Headers["X-GameLauncherHash"] = Value();
             request.Headers["X-DiscordID"] = Self.discordid;
             //request.Timeout = 30000;
