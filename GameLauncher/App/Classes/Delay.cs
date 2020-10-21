46e534a05707075cae072975759c924c23bb4023
DavidCarbon <davidlandaverdejr@gmail.com>
2020-10-21 14:18:42 -0700

    Revert "Revert "Code Cleanup (Moved to Gist)""
    This reverts commit c3946f161080b653b58563287d0c6f51ae3fd749.

--- GameLauncher/App/Classes/Delay.cs
+++ GameLauncher/App/Classes/Delay.cs
@@ -12,13 +12,8 @@ namespace GameLauncher.App.Classes {
             }
         }
 
-        public static void WaitMSeconds(int sec) {
-            if (sec < 1) return;
-            DateTime _desired = DateTime.Now.AddMilliseconds(sec);
-            while (DateTime.Now < _desired)
-            {
-                Application.DoEvents();
-            }
-        }
+        /* Moved "AddMilliseconds" Code to Gist */
+        /* https://gist.githubusercontent.com/DavidCarbon/97494268b0175a81a5f89a5e5aebce38/raw/15d5ef79b6c5e3dbca112a65b2a77623c5712411/Delay.cs */
+
     }
 }
--- GameLauncher/App/Classes/IniFile.cs
+++ GameLauncher/App/Classes/IniFile.cs
@@ -7,6 +7,9 @@ using IniParser.Model;
 
 namespace GameLauncher.App.Classes {
 	class IniFile {
+        /* Moved 2 Public functions to Gist */
+        /* https://gist.githubusercontent.com/DavidCarbon/97494268b0175a81a5f89a5e5aebce38/raw/89c2e19c97be7ebc075203f3d998aa9e701892f6/IniFile.cs */
+
         string Path;
 		string EXE = Assembly.GetExecutingAssembly().GetName().Name;
         FileIniDataParser Parser;
@@ -27,11 +30,6 @@ namespace GameLauncher.App.Classes {
             return Data[EXE][Key];
         }
 
-	    public int ReadInt(string Key)
-	    {
-	        return Convert.ToInt32(Data[EXE][Key]);
-	    }
-
         public void Write(string Key, string Value) {
 			try {
 				Data[EXE][Key] = Value;
@@ -46,11 +44,6 @@ namespace GameLauncher.App.Classes {
 			    Data[EXE].RemoveKey(Key);
                 Parser.WriteFile(Path, Data);
             } catch { }
-        }
-
-        public void DeleteSection(string Section) {
-			Data.Sections.RemoveSection(Section);
-            Parser.WriteFile(Path, Data);
         }
 
         public bool KeyExists(string Key) {
