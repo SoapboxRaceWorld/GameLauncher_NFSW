8a4e218452aecf14d1e72ad0337f920fdaef85dd
Zacam <Zacam.1369@gmail.com>
2020-11-04 21:45:00 -0800

    UnifiedTheme: UpdatePopup
    Part of the process to theme/unify and stylize as many aspects of the Forms and Windows used by the Launcher.

--- GameLauncher/App/UpdatePopup.Designer.cs
+++ GameLauncher/App/UpdatePopup.Designer.cs
@@ -42,9 +42,12 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(29)))), ((int)(((byte)(38)))));
             this.groupBox1.Controls.Add(this.changelogText);
-            this.groupBox1.Location = new System.Drawing.Point(12, 66);
+            this.groupBox1.Location = new System.Drawing.Point(12, 64);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(0);
             this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(0);
             this.groupBox1.Size = new System.Drawing.Size(374, 158);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
@@ -52,24 +55,33 @@
             // 
             // changelogText
             // 
-            this.changelogText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.changelogText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(29)))), ((int)(((byte)(38)))));
             this.changelogText.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.changelogText.Location = new System.Drawing.Point(6, 19);
+            this.changelogText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changelogText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(210)))), ((int)(((byte)(255)))));
+            this.changelogText.Location = new System.Drawing.Point(6, 13);
+            this.changelogText.Margin = new System.Windows.Forms.Padding(3, 0, 3, 3);
             this.changelogText.Multiline = true;
             this.changelogText.Name = "changelogText";
             this.changelogText.ReadOnly = true;
             this.changelogText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.changelogText.Size = new System.Drawing.Size(362, 133);
+            this.changelogText.Size = new System.Drawing.Size(368, 139);
             this.changelogText.TabIndex = 0;
+            this.changelogText.Text = "This is a Test";
             // 
             // update
             // 
+            this.update.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(29)))), ((int)(((byte)(38)))));
+            this.update.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(181)))), ((int)(((byte)(191)))));
+            this.update.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(58)))), ((int)(((byte)(76)))));
+            this.update.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.update.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.update.Location = new System.Drawing.Point(149, 230);
             this.update.Name = "update";
             this.update.Size = new System.Drawing.Size(75, 23);
             this.update.TabIndex = 2;
             this.update.Text = "Update";
-            this.update.UseVisualStyleBackColor = true;
+            this.update.UseVisualStyleBackColor = false;
             // 
             // icon
             // 
@@ -91,22 +103,32 @@
             // 
             // skip
             // 
+            this.skip.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(29)))), ((int)(((byte)(38)))));
+            this.skip.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(181)))), ((int)(((byte)(191)))));
+            this.skip.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(58)))), ((int)(((byte)(76)))));
+            this.skip.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.skip.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.skip.Location = new System.Drawing.Point(311, 230);
             this.skip.Name = "skip";
             this.skip.Size = new System.Drawing.Size(75, 23);
             this.skip.TabIndex = 6;
             this.skip.Text = "Skip";
-            this.skip.UseVisualStyleBackColor = true;
+            this.skip.UseVisualStyleBackColor = false;
             // 
             // ignore
             // 
+            this.ignore.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(29)))), ((int)(((byte)(38)))));
             this.ignore.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.ignore.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(181)))), ((int)(((byte)(191)))));
+            this.ignore.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(58)))), ((int)(((byte)(76)))));
+            this.ignore.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ignore.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.ignore.Location = new System.Drawing.Point(230, 230);
             this.ignore.Name = "ignore";
             this.ignore.Size = new System.Drawing.Size(75, 23);
             this.ignore.TabIndex = 1;
             this.ignore.Text = "Ignore";
-            this.ignore.UseVisualStyleBackColor = true;
+            this.ignore.UseVisualStyleBackColor = false;
             // 
             // UpdatePopup
             // 
@@ -114,21 +136,22 @@
             this.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuPopup;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(36)))), ((int)(((byte)(45)))));
             this.CancelButton = this.ignore;
             this.ClientSize = new System.Drawing.Size(398, 262);
             this.Controls.Add(this.skip);
             this.Controls.Add(this.updateLabel);
             this.Controls.Add(this.icon);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.update);
             this.Controls.Add(this.ignore);
-            this.Controls.Add(this.groupBox1);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "UpdatePopup";
-            this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Update Available";
