3efcc992950932e4c8116018625197b0e0f32184
DavidCarbon <open-source@davidcarbon.dev>
2020-11-08 15:44:58 -0800

    Add two additional Server Lists
    One Server additional Static Server List and WOPL serverlist only servers as a backup if all fails

--- GameLauncher/App/Classes/Self.cs
+++ GameLauncher/App/Classes/Self.cs
@@ -17,11 +17,15 @@ namespace GameLauncherReborn
         public static string mainserver = "https://api.worldunited.gg";
         public static string fileserver = "https://files.worldunited.gg";
         public static string staticapiserver = "http://api-sbrw.davidcarbon.download";
+        public static string secondstaticapiserver = "http://api2-sbrw.davidcarbon.download";
+        public static string woplserver = "http://worldonline.pl";
         public static string modnetserver = "http://cdn.soapboxrace.world";
 
         public static string[] serverlisturl = new string[] {
             mainserver + "/serverlist.json",
-            staticapiserver + "/serverlist.json"
+            staticapiserver + "/serverlist.json",
+            secondstaticapiserver + "/serverlist.json",
+            woplserver + "/serverlist.json"
         };
 
         public static string statsurl = mainserver + "/stats";
