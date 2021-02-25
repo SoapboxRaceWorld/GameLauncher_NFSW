7e60176f47c8eeea3c8fc798ef1353fd67d52f5d
Zacam <Zacam.1369@gmail.com>
2021-02-25 08:00:07 -0800

    Pre-Merge cleanup

--- GameLauncher/App/Classes/LauncherCore/InsiderKit/EnableInsider.cs
+++ GameLauncher/App/Classes/LauncherCore/InsiderKit/EnableInsider.cs
@@ -5,7 +5,7 @@ namespace GameLauncher.App.Classes.InsiderKit
         public static bool Insider = true;
 
         //Current month, day, year (2 digits), and letter! Ex: 12-15-20-A
-        public static string InsiderBuildNumber = "02-21-21-B";
+        public static string InsiderBuildNumber = "02-25-21-R";
 
         public static bool ShouldIBeAnInsider()
         {
