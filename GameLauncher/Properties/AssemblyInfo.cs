7aeb371d08b9a72b2641c51803e006f10a0e73b9
Zacam <zacam.1369@gmail.com>
2021-10-10 04:22:13 -0700

    Bump Build

--- GameLauncher/Properties/AssemblyInfo.cs
+++ GameLauncher/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("0a114c5c-3566-4a62-b05d-cba311988d8a")]
 
-[assembly: AssemblyVersion("2.1.8.9")]
-[assembly: AssemblyFileVersion("2.1.8.9")]
+[assembly: AssemblyVersion("2.1.9.01")]
+[assembly: AssemblyFileVersion("2.1.9.01")]
 [assembly: NeutralResourcesLanguage("en-US")]
