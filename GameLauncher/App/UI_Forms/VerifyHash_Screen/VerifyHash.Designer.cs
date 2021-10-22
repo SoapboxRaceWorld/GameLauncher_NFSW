8ae573443702760bb4584a21292d710049c8e8e3
Zacam <zacam.1369@gmail.com>
2021-10-21 22:13:01 -0700

    Update VerifyHash.Designer.cs
    Pin "Verify" version to the Left

--- GameLauncher/App/UI_Forms/VerifyHash_Screen/VerifyHash.Designer.cs
+++ GameLauncher/App/UI_Forms/VerifyHash_Screen/VerifyHash.Designer.cs
@@ -116,7 +116,7 @@
             this.VersionLabel.Size = new System.Drawing.Size(164, 14);
             this.VersionLabel.TabIndex = 5;
             this.VersionLabel.Text = "Version: vXX.XX.XX.XXXX";
-            this.VersionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.VersionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // VerifyHashWelcome
             // 
