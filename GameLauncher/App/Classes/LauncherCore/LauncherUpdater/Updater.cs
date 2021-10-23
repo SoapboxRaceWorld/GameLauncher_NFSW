7501da1bcaa1b8340e8974b464edd5e88d8eb849
DavidCarbon <open-source@davidcarbon.dev>
2021-10-23 10:40:37 -0700

    Format Change for Args for Updater

--- GameLauncher/App/Classes/LauncherCore/LauncherUpdater/Updater.cs
+++ GameLauncher/App/Classes/LauncherCore/LauncherUpdater/Updater.cs
@@ -156,8 +156,8 @@ namespace GameLauncher.App.Classes.LauncherCore.LauncherUpdater
                             string UpdaterPath = Strings.Encode(Path.Combine(Locations.LauncherFolder, Locations.NameUpdater));
                             if (File.Exists(UpdaterPath))
                             {
-                                Process.Start(UpdaterPath, Process.GetCurrentProcess().Id.ToString() + 
-                                    (EnableInsiderBetaTester.Allowed() || EnableInsiderDeveloper.Allowed() ? " Preview" : " Stable"));
+                                Process.Start(UpdaterPath, Process.GetCurrentProcess().Id.ToString() + " " +
+                                    (EnableInsiderBetaTester.Allowed() || EnableInsiderDeveloper.Allowed() ? "Preview" : "Stable"));
                             }
                             else
                             {
