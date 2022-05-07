9f31aa70b47ad8d081ab558578fbb8a06cafeab9
DavidCarbon <open-source@davidcarbon.dev>
2022-05-06 22:30:43 -0700

    Have LZMA be the default Downloader

--- SBRW.Launcher.Net/App/Classes/LauncherCore/Global/FunctionStatus.cs
+++ SBRW.Launcher.Net/App/Classes/LauncherCore/Global/FunctionStatus.cs
@@ -37,7 +37,11 @@ namespace SBRW.Launcher.App.Classes.LauncherCore.Global
         public static bool EnableInsiderPreview { get { return Save_Settings.Live_Data.Launcher_Insider == "1"; } }
         public static bool EnableThemeSupport { get { return Save_Settings.Live_Data.Launcher_Theme_Support == "1"; } }
         public static bool EnableLegacyTimer { get { return Save_Settings.Live_Data.Launcher_Legacy_Timer == "1"; } }
+#if DEBUG
         public static bool EnableLZMADownloader { get { return Save_Settings.Live_Data.Launcher_LZMA_Downloader == "1"; } }
+#else
+        public static bool EnableLZMADownloader { get { return Save_Settings.Live_Data.Launcher_LZMA_Downloader != "1"; } }
+#endif
     }
 
     /* This is Used to call Certain Functions (Such as Completion Status or Function Callbacks) */
