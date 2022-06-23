181d28d59924bf327088f61f9afde7f7ad8b0d69
DavidCarbon <open-source@davidcarbon.dev>
2022-06-22 23:27:23 -0700

    Cleanup Project File

--- SBRW.Launcher.Net/Properties/Resources.Designer.cs
+++ SBRW.Launcher.Net/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace SBRW.Launcher.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -57,26 +57,6 @@ namespace SBRW.Launcher.Properties {
             }
             set {
                 resourceCulture = value;
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap icon_close {
-            get {
-                object obj = ResourceManager.GetObject("icon_close", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap information_window_outline_error {
-            get {
-                object obj = ResourceManager.GetObject("information_window_outline_error", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
