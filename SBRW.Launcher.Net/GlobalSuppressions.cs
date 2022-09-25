8cb1334097e420ac598c78f14d22ede1991b93f7
Zacam <zacam.1369@gmail.com>
2022-09-25 04:17:03 -0700

    Minor Fix, Bump Build Date
    Fix Suppression of loweCase on Updater GlobalSuppressions
    Bump Build Date

--- SBRW.Launcher.Net/GlobalSuppressions.cs
+++ SBRW.Launcher.Net/GlobalSuppressions.cs
@@ -5,6 +5,6 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed within API/Updater Checks", Scope = "namespaceanddescendants", Target = "~P:SBRW.Launcher.RunTime.LauncherCore.LauncherUpdater.LauncherUpdateCheck.status")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed within API/Updater Checks", Scope = "namespaceanddescendants", Target = "~N:SBRW.Launcher.RunTime.LauncherCore.LauncherUpdater")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Server JSON uses lowerCase", Scope = "namespaceanddescendants", Target = "~N:SBRW.Launcher.RunTime.LauncherCore.ModNet.JSON")]
 [assembly: SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "Inline Pragma's needed for multi-build", Scope = "module")]
