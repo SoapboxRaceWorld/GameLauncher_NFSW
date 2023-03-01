0a3e504081c446d474b036d34e6eced606c1be24
Zacam <zacam.1369@gmail.com>
2023-03-01 01:35:58 -0800

    Build Point
    Update Version Info

--- SBRW.Launcher.RunTime/InsiderKit/KitEnabler.cs
+++ SBRW.Launcher.RunTime/InsiderKit/KitEnabler.cs
@@ -8,7 +8,7 @@ namespace SBRW.Launcher.RunTime.InsiderKit
     {
         /* Current month, day, year (2 digits), and letter! Ex: 12-15-20-A */
         /* If a second build gets release within the same day bump letter version up (No R2 or D2)*/
-        private static string InsiderBuildNumber { get; set; } = "02-28-23-A";
+        private static string InsiderBuildNumber { get; set; } = "03-01-23-A";
 
         public static string BuildNumberOnly()
         {
